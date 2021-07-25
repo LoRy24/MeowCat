eaa95b0bd8c503184e1fdbe088131fe3e7d59382
LoRy24 <lorytherock24tutorial@gmail.com>
2021-07-25 11:02:23 +0200

    Added new features

---
+++ images.js
@@ -0,0 +1,11 @@
+module.exports = {
+    random_images: [
+        "https://i1.sndcdn.com/artworks-RJG4jIrv6lZwWkQH-cWi6nA-t500x500.jpg",
+        "https://static.wikia.nocookie.net/meme/images/5/52/Polite_Cat-0.jpg/revision/latest?cb=20200611120017",
+        "https://www.cnet.com/a/img/S8WsucQh6wWeUG1yrQi66jKNtto=/940x0/2020/09/22/ad4bd31b-cf8c-46f5-aa70-231df9acc041/longcat.jpg",
+        "https://assets3.thrillist.com/v1/image/2824030/828x610/flatten;crop;jpeg_quality=70",
+        "https://i.redd.it/qj0rwbqrkj841.jpg",
+        "https://pbs.twimg.com/profile_images/1340901366873636866/C_M2FWjw_400x400.jpg",
+    ],
+    hacker_cat: "https://www.superagency.net/appwp19/wp-content/uploads/2021/02/6c90288d7e10d46d18895f17f420a92c.gif"
+}
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const Discord = require("discord.js");
-const SecurityValues = require("security_values");
+const SecurityValues = require("./security_values.js");
+const Images = require("./images.js")
 const bot = new Discord.Client();
 
-bot.on("message", () => {
+bot.on("message", (message) => {
+    let words = message.content.toLowerCase().split(" ");
+    if (message.author === bot.user) return;
 
+    let imageEmbed = new Discord.MessageEmbed().setTitle("Meow").setColor("ORANGE")
+        .setFooter("Bot made by LoRy24");
+    if (words.includes("meow")) {
+        message.channel.send(imageEmbed.setImage(Images.random_images[Math.floor(Math.random() * Images
+            .random_images.length)])).then(() => {});
+    } else if (words.includes("hacker-meow")) {
+        message.channel.send(imageEmbed
+            .setImage(Images.hacker_cat).setTitle("HACKER MEOW"))
+            .then(() => {});
+    }
 });
 
 bot.login(SecurityValues.bot_token).then(() => {
